a5d9bb50813119e13e0281042b5177ae8db2cf4b
Aaron <aaron@sphericalwave.com>
2026-07-03 18:06:16 -0300

    chore: lower iOS floor to 16 (breathe's deployment target)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "HeartRateKit",
-    platforms: [.iOS(.v17), .macOS(.v14)],
+    platforms: [.iOS(.v16), .macOS(.v14)],
     products: [
         .library(name: "HeartRateKit", targets: ["HeartRateKit"]),
     ],
